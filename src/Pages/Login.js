8ad193c5f8c9ddbc3e12d9e7ff96a3af3c1de69c
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2024-02-13 13:35:39 -0600

    add buttons

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
+import "./Login.css"
+
+function loginButton()
+{
+  alert('You clicked me!')
+}
+
+function registerButton()
+{
+  alert('You clicked me!')
+}
 
 export default function Login() {
   return (
     <div>
-        Login
+      <h1>Login</h1>
+      <center><button onClick={loginButton}>Login</button></center>
+      <center><button onClick={registerButton}>Register</button></center>
     </div>
   )
 }
