cb77211d1b76f2bcabce6a85a8df173df4ce1232
RaianM <momin.raian@gmail.com>
2024-02-13 13:54:36 -0600

    Added Initial Routes to Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
 
 function App() {
   return (
     <div>
-      <Login />
-      <Profile />
+		<Routes>
+			<Route path="/" element={<Login />}/>
+			<Route index element={<Login />} />
+			<Route path="/Profile" element={<Profile />} />
+		</Routes>
     </div>
   );
 }
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,22 +2,25 @@ import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import "./Login.css"
 
-function loginButton()
-{
-  alert('You clicked me!')
-}
-
-function registerButton()
-{
-  alert('You clicked me!')
-}
-
 export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <center><button onClick={loginButton}>Login</button></center>
-      <center><button onClick={registerButton}>Register</button></center>
-    </div>
+    function loginButton()
+    {
+        alert('You clicked me!')
+    }
+
+    function registerButton()
+    {
+        alert('You clicked me!')
+    }
+    return (
+        <div>
+        <h1>Login</h1>
+        <center>
+            <Link to="/Profile">
+                <button onClick={loginButton}>Login</button>
+            </Link>
+        </center>
+        <center><button onClick={registerButton}>Register</button></center>
+        </div>
   )
 }
