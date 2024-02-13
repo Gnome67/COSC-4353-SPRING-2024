9e684cb70aa7691cb31cb10dba6a5278612c323e
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2024-02-13 14:21:47 -0600

    homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
 import Register from "./Pages/Register";
@@ -7,7 +8,7 @@ function App() {
   return (
     <div>
 		<Routes>
-			<Route path="/" element={<Login />}/>
+			<Route path="/" element={<Home />}/>
 			<Route path="/Login" element={<Login />} />
 			<Route path="/Profile" element={<Profile />} />
 			<Route path="/Register" element={<Register />} />
