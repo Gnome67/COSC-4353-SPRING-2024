b2279a58c46eaee33113f0bfd6cafef82ba6ea62
HOU-Noah-Harrison <91997693+noahhhtx@users.noreply.github.com>
2024-02-18 21:08:36 -0600

    added fuel quote form page

---
+++ src/Pages/fuelQuoteForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom';
+import "./fuelQuoteForm.css"
+
+export default function fuelQuoteForm() {
+    return (
+        <body></body>
+    )
+}
