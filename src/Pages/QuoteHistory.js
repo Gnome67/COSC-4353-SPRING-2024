97c3ea5db89bdb5f63f4841c8625ea55e7690a55
tahaamir25 <156965121+tahaamir25@users.noreply.github.com>
2024-02-19 21:24:34 -0600

    practice (text change)

--- src/Pages/QuoteHistory.js
+++ src/Pages/QuoteHistory.js
@@ -4,7 +4,7 @@ import "./QuoteHistory.css"
 
 // Message from Raian: Feel free to remove the <p> tag, but keep the <div> and have 
 // all your html needs to be inside that <div> tag, you can add an id or className if you need it
-// You can remove these comments too :)
+// You can remove these comments too :) hey
 
 export default function QuotePage() {
     return (
