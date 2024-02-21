ab8ae824b745fdfdb31205f671b62109c6a10e53
tahaamir25 <156965121+tahaamir25@users.noreply.github.com>
2024-02-21 17:09:02 -0600

    QuoteHistory Attempt 1

--- src/Pages/QuoteHistory.js
+++ src/Pages/QuoteHistory.js
@@ -9,7 +9,32 @@ import "./QuoteHistory.css"
 export default function QuotePage() {
     return (
         <div>
-            <p>This is the Quote History Page</p> 
+            <h1>Quote History</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Quote ID</th>
+                        <th>Customer</th>
+                        <th>Gallons Requested</th>
+                        <th>Delivery Address</th>
+                        <th>Delivery Date</th>
+                        <th>Suggested Price</th>
+                        <th>Total Amount Due</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Taha Amir</td>
+                        <td>5</td>
+                        <td>251 Green Street</td>
+                        <td>2024-02-20</td>
+                        <td>$3</td>
+                        <td>2024-02-20</td>
+                    </tr>
+                    {/* More Rows */}
+                </tbody>
+            </table>
         </div>
     )
 }
