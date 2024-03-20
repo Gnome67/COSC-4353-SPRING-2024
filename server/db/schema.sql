f8265a620a40b90f41132fc3a5a162c9dc9705a9
tahaamir25 <156965121+tahaamir25@users.noreply.github.com>
2024-03-20 12:48:07 -0500

    Update schema.sql

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -26,3 +26,15 @@ CREATE TABLE profile (
 -- Quote Table
 
 -- Sessions Table
+create table sessions (
+		username varchar(16) not null,
+        token varchar(512) not null,
+        primary key(username, token),
+        foreign key(username) references users(username)
+);
+commit;
+-- Create a role 'app' and login for the backend to use on the database
+create user 'app' identified by 'test_password';
+-- Granting necessarily data-modification privileges to 'app'
+grant insert, delete, select, update on cosc4353app.* to 'app';
+commit;
