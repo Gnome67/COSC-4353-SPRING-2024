b3f44363f4fa6294d2f043aafea95a18351749a3
tahaamir25 <156965121+tahaamir25@users.noreply.github.com>
2024-03-25 15:01:48 -0500

    Taha Part

--- server/src/PricingModule.js
+++ server/src/PricingModule.js
@@ -1,25 +1,43 @@
+const { knexClient } = require('./knexClient')
+
 class FuelDelivery {
-  constructor(
-    gallonsRequested,
-    deliveryDate,
-    deliveryAddress,
-    deliveryCity,
-    deliveryState,
-    deliveryZipcode,
-    suggestedPricePerGallon
-  ) {
-    this.gallonsRequested = gallonsRequested;
-    this.deliveryDate = deliveryDate;
-    this.deliveryAddress = deliveryAddress;
-    this.deliveryCity = deliveryCity;
-    this.deliveryState = deliveryState;
-    this.deliveryZipcode = deliveryZipcode;
-    this.suggestedPricePerGallon = suggestedPricePerGallon;
+  constructor(gallons, address, city, state, zipcode, username) {
+    this.gallons = gallons
+    this.address = address
+    this.city = city
+    this.state = state
+    this.zipcode = zipcode
+    this.username = username
   }
 
-  getTotalAmountDue() {
-    return this.gallonsRequested * this.suggestedPricePerGallon;
+  async getPricePerGallon() {
+    const locationFactor = this.state === 'TX' ? 0.02 : 0.04
+    const gallonsRequestedFactor = this.gallons > 1000 ? 0.02 : 0.03
+    const companyProfitFactor = 0.1
+
+    const quoteHistory = await knexClient('quote').select().where('client_username', '=', this.username)
+    const rateHistoryFactor = quoteHistory.length > 0 ? 0.01 : 0
+
+    const currentPrice = 1.5
+    const margin =
+      currentPrice *
+      (locationFactor -
+        rateHistoryFactor +
+        gallonsRequestedFactor +
+        companyProfitFactor)
+
+    const suggestedPrice = currentPrice + margin
+
+    return suggestedPrice
+  }
+
+  async getTotalAmountDue() {
+    const suggestedPricePerGallon = await this.getPricePerGallon()
+    if (this.gallons <= 0) {
+      return NaN
+    }
+    return this.gallons * suggestedPricePerGallon
   }
 }
 
-module.exports = FuelDelivery;
+module.exports = FuelDelivery
