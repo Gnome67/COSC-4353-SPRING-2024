805f696ace80508a2a5d9234f3bc9556bf034637
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2024-03-27 22:49:04 -0500

    Update login.test.js

--- server/src/login.test.js
+++ server/src/login.test.js
@@ -1,81 +1,90 @@
+const { knexClient } = require('./knexClient');
+const { generate_token, validate_token, invalidate_token, create_user } = require('./login');
+
 const valid_user = {
     username: 'talha',
-    password: 'test2',
+    password: 'FancyTest12',
+    hashed_password: '3J9YmiYMzPzLfr3h96c4O/vKGjZuDwhpJo05wSJOZls='
 };
 const invalid_user = {
-    username: 'eve',
-    password: 'badpass',
+    username: 'talha',
+    password: 'DubiousPassword13',
 };
 const bad_password = {
     username: 'talha',
-    password: 'wrongpass',
+    password: 'NotRightPassword13',
 };
 const fake_token = 'obviousfaketoken';
-const other_user = 'taha';
+const other_user = 'talha';
 
-const { generate_token, validate_token, invalidate_token } = require('./login');
+const db_cleanup = () => knexClient.transaction(async trx => {
+    await trx('sessions').del();
+    await trx('profile').del();
+    await trx('quote').del();
+    await trx('users').del();
+});
+
+beforeEach(() => db_cleanup());
+afterEach(() => db_cleanup());
+afterAll(() => knexClient.destroy());
 
 describe('The login module', () => {
-    test('should load', () => {
-        
+    test('sanity check', () => {
+        expect(1+1).toBe(2);
+    });
+    test('should load', async () => {
         expect(generate_token).not.toBe(undefined);
     });
-    test('allows valid users to log in', () => {
-        let token = undefined;
-        try {
-            token = generate_token(valid_user.username, valid_user.password);
-        } catch (e) {
-            token = undefined;
-        }
-        expect(token).not.toBeUndefined();
+    test('allows valid username and password for registration', async () => {
+        return expect(create_user(valid_user.username, valid_user.password)).resolves.not.toBeUndefined();
     });
-    test('forbid nonexistent users to log in', () => {
-        let token = undefined;
-        try {
-            token = generate_token(invalid_user.username, invalid_user.password);
-        } catch (e) {
-            token = undefined;
-        }
-        expect(token).toBeUndefined();
+    test('forbids illegal characters in usernames', async () => {
+        return expect(create_user('it\'sme!', 'This1SecurePass')).rejects.toThrow();
     });
-    test('returns an error for incorrect password', () => {
-        let token = undefined;
-        try {
-            token = generate_token(bad_password.username, bad_password.password);
-        } catch (e) {
-            token = undefined;
-        }
-        expect(token).toBeUndefined();
+    test('forbids usernames to be too short', async () => {
+        return expect(create_user('a', 'This1SecurePass')).rejects.toThrow();
     });
-    test('generates valid tokens on successful login', () => {
-        let token = undefined
-        try {
-            token = generate_token(valid_user.username, valid_user.password);
-        } catch (e) {
-            token = undefined;
-        }
-        expect(validate_token(valid_user.username, token)).toBe(true);
+    test('allows valid users to log in', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        return expect(generate_token(valid_user.username, valid_user.password)).resolves.not.toBeUndefined();
     });
-    test('does not validate forged tokens', () => {
-        expect(validate_token(valid_user.username, fake_token)).toBe(false);
+    test('forbid nonexistent users to log in', async () => {
+        return expect(generate_token(invalid_user.username, invalid_user.password)).rejects.toThrow();
     });
-    test('throws error if logout is attempted on an invalid token', () => {
-        expect(() => invalidate_token(valid_user.username, fake_token)).toThrow();
+    test('returns an error for incorrect password', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        return expect(generate_token(bad_password.username, bad_password.password)).rejects.toThrow();
     });
-    test('successfully invalidates a valid token on log out', () => {
-        const token = generate_token(valid_user.username, valid_user.password);
-        expect(token).not.toBeUndefined();
-        const was_invalidated = invalidate_token(valid_user.username, token);
+    test('generates valid tokens on successful login', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        return expect(generate_token(valid_user.username, valid_user.password).then((token) => {
+            return validate_token(valid_user.username, token)
+        })).resolves.toBe(true);
+    });
+    test('does not validate forged tokens', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        return expect(validate_token(valid_user.username, fake_token)).resolves.toBe(false);
+    });
+    test('throws error if logout is attempted on an invalid token', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        return expect(invalidate_token(valid_user.username, fake_token)).rejects.toThrow();
+    });
+    test('successfully invalidates a valid token on log out', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        const token = await generate_token(valid_user.username, valid_user.password);
+        const was_invalidated = await invalidate_token(valid_user.username, token);
         expect(was_invalidated).toBe(true);
-        expect(validate_token(valid_user.username, token)).toBe(false);
-        expect(() => invalidate_token(valid_user.username, token)).toThrow();
+        //return expect(invalidate_token(valid_user.username, token)).rejects.toThrow();
+        return expect(invalidate_token(valid_user.username, token)).rejects.toThrow();
     });
-    test('does not allow log in as a different user with a valid token', () => {
-        const token = generate_token(valid_user.username, valid_user.password);
-        expect(validate_token(other_user, token)).toBe(false);
+    test('does not allow log in as a different user with a valid token', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        const token = await generate_token(valid_user.username, valid_user.password);
+        return expect(await validate_token(other_user, token)).toBe(false);
     });
-    test('does not allow invalidation of another user\'s token', () => {
-        const token = generate_token(valid_user.username, valid_user.password);
-        expect(() => invalidate_token(other_user, token)).toThrow();
+    test('does not allow invalidation of another user\'s token', async () => {
+        await create_user(valid_user.username, valid_user.password);
+        const token = await generate_token(valid_user.username, valid_user.password);
+        return expect(invalidate_token(other_user, token)).rejects.toThrow();
     });
 });
