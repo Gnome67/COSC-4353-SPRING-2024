6c02688ba943871a5ece997bb17f491203644996
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2024-03-27 22:48:00 -0500

    Update login.js

--- server/src/login.js
+++ server/src/login.js
@@ -1,60 +1,121 @@
 // Login module skeleton
+const { initializeQuoteHistory } = require('./fuelquotes')
 const jwt = require('jsonwebtoken');
+const SHA256 = require('crypto-js/sha256');
+const Base64 = require('crypto-js/enc-base64');
+const { knexClient } = require('./knexClient');
 
 // TODO make this crypto secure!
 const token_secret = 'mysecretkeylol';
 
-// "Hard"-coded storage of username/passwords until we implement database
-var users = new Map();
-users.set('raian', 'test1');
-users.set('talha', 'test2');
-users.set('noah', 'test3');
-users.set('taha', 'test4');
+const username_validate = (username) => {
+    // Validate the the username does not include any illegal characters and is the required length
+    return username.match(/^[a-zA-z0-9]{3,}$/)
+};
 
-var invalid_sessions = new Map();
+const password_validate = (password) => {
+    return password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)
+};
 
-const generate_token = (username, password) => {
+const is_valid = async (username, password) => {
+    return knexClient.select('password').where('username', '=', username).from('users')
+        .then((rows) => {
+            if (rows.length !== 1) {
+                return false;
+            }
+            const db_hash = rows[0]['password'];
+            const passed_hash = SHA256(password).toString(Base64);
+            if (db_hash !== passed_hash) {
+                return false;
+            }
+            return true;
+        });
+}
+
+const username_exists = async (username) => {
+    return knexClient.select('username').where('username', '=', username).from('users')
+        .then((rows) => { return rows.length > 0 });
+}
+
+const useradd = async (username, password) => {
+    return knexClient('users').insert({username, password: SHA256(password).toString(Base64)}).then((_) => { 
+    }).catch((e) => { throw e });
+}
+
+const make_invalid = async (username, token) => {
+    knexClient('sessions').insert({username, token})
+        .catch((e) => {throw e});
+}
+
+const token_is_invalid = async (token) => {
+    if (!token) {
+        return false;
+    }
+    const rows = await knexClient('sessions').select().where('token', '=', token)
+    if (rows.length !== 0) {
+        return true;
+    }
+    return false;
+}
+
+// TODO need to add input validation
+const generate_token = async (username, password) => {
     // Validate that the username and password are in agreement
     // TODO implement a database lookup to do this
-    const is_valid = users.get(username) === password;
-    if (!is_valid) {
+    //const is_valid = users.get(username) === password;
+    //if (!is_valid) {
+    const valid_user = await is_valid(username, password);
+    if (!valid_user) {
         throw new Error('Invalid user or password');
     }
     const token = jwt.sign({ username: username}, token_secret, { expiresIn: 1800 });
-    invalid_sessions.delete(token);
     return token;
 };
 
-const validate_token = (username, token) => {
-    if (invalid_sessions.has(token)) {
+const validate_token = async (username, token) => {
+    if (await token_is_invalid(token)) {
         return false;
     }
-    else {
-        try {
-            const payload = jwt.verify(token, token_secret, { expiresIn: 1800 });
-            return username === payload.username;
-        }
-        catch (e) {
-            return false;
-        }
+    try {
+        const payload = jwt.verify(token, token_secret, { expiresIn: 1800 });
+        return username === payload.username;
+    }
+    catch (e) {
+        return false;
     }
 };
 
-const invalidate_token = (username, token) => {
-    if (invalid_sessions.has(token)) {
-        throw Error('Cannot invalidate an invalid token!');
-    }
+const invalidate_token = async (username, token) => {
     try {
         const payload = jwt.verify(token, token_secret, { expiresIn: 1800 });
         if (username !== payload.username) {
             throw Error('Cannot invalidate an invalid token!');
         }
-        invalid_sessions.set(token, 1);
-    }
-    catch (e) {
+        if (await token_is_invalid(token)) {
+            throw Error('Cannot invalidate an invalid token!');
+        }
+        await make_invalid(username, token);
+        return true;
+    } catch (e) {
         throw Error('Cannot invalidate an invalid token!');
     }
-    return true;
 };
 
-module.exports = { generate_token, validate_token, invalidate_token };
+const create_user = async (username, password) => {
+    if (await username_exists(username)) {
+        throw Error('User already exists');
+    } else if (!username_validate(username)) {
+        throw Error('Username contains illegal characters or is the wrong length');
+    } else if (!password_validate(password)) {
+        throw Error('Password contains illegal characters or is not the correct length');
+    }
+    try {
+        await useradd(username, password)
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+    return true;
+}
+
+module.exports = { generate_token, validate_token, invalidate_token, create_user };
